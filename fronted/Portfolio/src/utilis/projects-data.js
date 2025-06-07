d341de7e6036ede36cbae9adc74c7eaf49768c03
Harsh shukla <harshshukla844277@gmail.com>
2025-06-07 12:15:18 +0530

    project add

--- fronted/Portfolio/src/utilis/projects-data.js
+++ fronted/Portfolio/src/utilis/projects-data.js
@@ -1,23 +1,34 @@
 export const projectsData = [
-    {
-        id: 1,
-        name: 'Study Notion - Ed Tech Platform',
-        description: "Study Notion is a fully-featured EdTech platform where instructors can upload and manage courses, and students can enroll, make payments, and track progress. It includes features like course creation, video uploading via Cloudinary, secure payments through Razorpay, authentication with JWT, and role-based dashboards for both instructors and learners.",
-        tools: ['React', 'Node.js', 'Express', 'MongoDB', 'Tailwind CSS', 'Redux', 'JWT', 'Razorpay'],
-        role: 'Full Stack Developer',
-        code: 'https://github.com/HarshShukla77/StudyNotion',  
-        demo: 'https://github.com/HarshShukla77/StudyNotion', 
-      },
-      {
-        id: 2,
-        name: 'Real-Time Chat App',
-        description: 'A real-time chat application built using Socket.io and Node.js where users can sign up, log in, and chat instantly in private or group chats. It supports features like online status, typing indicators, message timestamps, and responsive UI. Messages are stored securely in MongoDB, and the app includes JWT-based authentication for protected routes.',
-        tools: ['React', 'Node.js', 'Express', 'Socket.io', 'MongoDB', 'Tailwind CSS', 'ChakraUI'],
-        role: 'Full Stack Developer',
-        code: 'https://github.com/HarshShukla77/RealTimeChatApp', 
-        demo: 'https://talkative-i3rq.onrender.com', 
-        
-      },
-     
-      
+  {
+    id: 1,
+    name: 'Study Notion - Ed Tech Platform',
+    description: "Study Notion is a fully-featured EdTech platform where instructors can upload and manage courses, and students can enroll, make payments, and track progress. It includes features like course creation, video uploading via Cloudinary, secure payments through Razorpay, authentication with JWT, and role-based dashboards for both instructors and learners.",
+    tools: ['React', 'Node.js', 'Express', 'MongoDB', 'Tailwind CSS', 'Redux', 'JWT', 'Razorpay'],
+    role: 'Full Stack Developer',
+    code: 'https://github.com/HarshShukla77/StudyNotion',
+    demo: 'https://github.com/HarshShukla77/StudyNotion',
+  },
+  {
+    id: 2,
+    name: 'Real-Time Chat App',
+    description: 'A real-time chat application built using Socket.io and Node.js where users can sign up, log in, and chat instantly in private or group chats. It supports features like online status, typing indicators, message timestamps, and responsive UI. Messages are stored securely in MongoDB, and the app includes JWT-based authentication for protected routes.',
+    tools: ['React', 'Node.js', 'Express', 'Socket.io', 'MongoDB', 'Tailwind CSS', 'ChakraUI'],
+    role: 'Full Stack Developer',
+    code: 'https://github.com/HarshShukla77/RealTimeChatApp',
+    demo: 'https://talkative-i3rq.onrender.com',
+
+  },
+  {
+    id: 3,
+    name: 'Mystry Message - Feedback Sharing Platform',
+    description:
+      'Mystry Message is an anonymous feedback and message sharing platform where users can register, receive mystery messages, and manage them securely. It includes authentication via NextAuth, protected API routes, MongoDB database integration with Mongoose, and a user-friendly UI built with Tailwind CSS. Deployed on Vercel for fast performance and scalability.',
+    tools: ['Next.js', 'Shadcn', 'MongoDB', 'Mongoose', 'NextAuth.js', 'Tailwind CSS'],
+    role: 'Full Stack Developer',
+    code: 'https://github.com/HarshShukla77/feedback', 
+    demo: 'https://feedback-f8qamgqxn-harshshukla77s-projects.vercel.app/',
+  },
+
+
+
 ]
